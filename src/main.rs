c679512a70922a614bb5e97478fb0c60896d569d
Nikolay Koshkin <koshkin.kna@gmail.com>
2021-07-11 00:10:14 +0300

    remove idea folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+
+use rand::Rng;
+
+
+
+
+
 fn main() {
     println!("Hello, world!");
+    let mut vec_number: Vec<i32> = Vec::new();
+    let mut rnd = rand::thread_rng();
+    for i in 0..10 { vec_number.push(rnd.gen_range(0..100)) }
+
+
+    println!("{:?}", vec_number);
 }
