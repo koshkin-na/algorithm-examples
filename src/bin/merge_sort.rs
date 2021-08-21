f8fb085e0c342a05d528221294f9e02e3b851d1d
Nikolay Koshkin <koshkin.kna@gmail.com>
2021-08-21 10:47:09 +0000

    optimization merge_sort.rs

--- src/bin/merge_sort.rs
+++ src/bin/merge_sort.rs
@@ -4,10 +4,8 @@ use std::time::Duration;
 use cpu_time::ProcessTime;
 use simplelog::{Config, SimpleLogger};
 
-
 const RANGE_BEHCMARK: i64 = 100_000_000;
-// const RANGE_BEHCMARK: i64 = 100;
-
+// const RANGE_BEHCMARK: i64 = 10;
 
 fn main() {
     let _ = SimpleLogger::init(log::LevelFilter::Info, Config::default());
@@ -15,63 +13,61 @@ fn main() {
     
     let mut vec_number: Vec<i32> = Vec::new();
     let mut rnd = rand::thread_rng();
-    for _ in 0..RANGE_BEHCMARK {
-        vec_number.push(rnd.gen_range(0..100))
-    }
-    if vec_number.len() < 20 {
-        log::info!("{:?}", vec_number);
-    }
+    for _ in 0..RANGE_BEHCMARK { vec_number.push(rnd.gen_range(0..1000)) }
+    let mut vec_number_clone = vec_number.clone();
+    if vec_number.len() < 20 { log::info!("{:?}", vec_number); }
 
     let start = ProcessTime::now();
-    let res = merge_sort(&vec_number);
+    merge_sort(&mut vec_number);
     let cpu_time: Duration = start.elapsed();
     log::info!("merge_sort() CPU time elapsed is {:?}", cpu_time);
-    if vec_number.len() < 20 {
-        log::info!("{:?}", res);
-    }
+    if vec_number.len() < 20 { log::info!("{:?}", vec_number); }
 
     let start = ProcessTime::now();
-    vec_number.sort();
+    vec_number_clone.sort();
     let cpu_time: Duration = start.elapsed();
     log::info!("native Vector::sort() CPU time elapsed is  {:?}", cpu_time);
 }
 
-fn merge_sort<T: Ord + Sized + Debug + Clone + Copy>(array: &[T]) -> Vec<T> {
-    log::debug!("call merge_sort({:?})", array);
-    if array.len() > 1 {
-        let part_left = merge_sort(&array[0 .. (array.len() / 2)]);
-        let part_right = merge_sort(&array[(array.len() / 2) .. ]);
-        return merge(&part_left, &part_right)
-
-    } else {
-        return array.to_vec()
-    }
+fn merge_sort<T: Ord + Copy + Debug>(arr: &mut [T]) {
+    log::debug!("call merge_sort({:?})", arr);
+    let arr_len = arr.len();
+    let mid = arr_len / 2;
+    if arr_len <= 1 { return }
+    merge_sort(&mut arr[0..mid]);
+    merge_sort(&mut arr[mid..arr_len]);
+    let mut res: Vec<T> = arr.to_vec();
+    merge(
+        &arr[0..mid], 
+        &arr[mid..arr_len], 
+        &mut res[..]
+    );
+    arr.copy_from_slice(&res);
 }
 
-fn merge<T: Ord + Sized + Debug + Clone + Copy>(part_left: &[T], part_right: &[T]) -> Vec<T> {
-    log::debug!("call merge({:?}, {:?})", part_left, part_right);
-    let mut result = Vec::new();
-    let mut left_i: usize = 0;
-    let mut right_i: usize = 0;
-    loop {
-        if part_left.len() - 1 < left_i || part_right.len() - 1 < right_i { break }
-        if part_left[left_i] < part_right[right_i] {
-            result.push(part_left[left_i]);
-            left_i += 1;
+fn merge<T: Copy + PartialOrd + Debug>(x1: &[T], x2: &[T], y: &mut [T]) {
+    log::debug!("call merge({:?}, {:?}, {:?})", x1, x2, y);
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+    while i < x1.len() && j < x2.len() {
+        if x1[i] < x2[j] {
+            y[k] = x1[i];
+            k += 1;
+            i += 1;
         } else {
-            result.push(part_right[right_i]);
-            right_i += 1;
+            y[k] = x2[j];
+            k += 1;
+            j += 1;
         }
     }
-    if part_left.len() > left_i {
-        result.append(&mut part_left[left_i..].to_vec())
+    if i < x1.len() {
+        y[k..].copy_from_slice(&x1[i..]);
     }
-    if part_right.len() > right_i {
-        result.append(&mut part_right[right_i..].to_vec())
+    if j < x2.len() {
+        y[k..].copy_from_slice(&x2[j..]);
     }
-    return result
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -80,11 +76,12 @@ mod tests {
         use rand::Rng;
         let mut vec_number: Vec<i32> = Vec::new();
         let mut rnd = rand::thread_rng();
-        for _ in 0..100 {
+        for _ in 0..10000 {
             vec_number.push(rnd.gen_range(0..100))
         }
-        let res = crate::merge_sort(&vec_number);
-        vec_number.sort();
-        assert_eq!(res, vec_number);
+        let mut vec_number_clone = vec_number.clone();
+        crate::merge_sort(&mut vec_number);
+        vec_number_clone.sort();
+        assert_eq!(vec_number, vec_number_clone);
     }
 }
